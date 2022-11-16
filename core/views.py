43cc17431dc1ed25b645bc31cf0bae5f366ee087
Dcampos <daniel.campos@gmx.us>
2022-11-16 16:39:21 -0300

    added login and signup pages

--- core/views.py
+++ core/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/home.html')
+
+def login(request):
+    return render(request, 'core/login.html')
+
+def signup(request):
+    return render(request, 'core/signup.html')
--- itrabs/urls.py
+++ itrabs/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
